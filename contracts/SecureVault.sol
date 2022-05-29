ac536a9502107a0a607c4ea8706817f0c5f108e0
Nnaemeka Umeh (fps) <anthony.nnaemeka.umeh@gmail.com>
2022-05-29 18:13:43 +0100

    Update SecureVault.sol

--- contracts/SecureVault.sol
+++ contracts/SecureVault.sol
@@ -17,22 +17,16 @@ contract SecureVault
 {
     // Using all the SafeMath for all uint256 calculations.
     using SafeMath for uint256;
-
     // Owner of the contract.
     address payable immutable owner;
-
     // Total money in the contract storage.
     uint256 private total_funds;
-
     // Default timespan constant for Rinkeby.
     uint256 constant DEFAULT_DURATION = 7 days;
-
     // Default timespan for Ganache.
     // uint256 constant DEFAULT_DURATION = 60 seconds;
-
     // Variable time for the user to add to the contract;
     uint256 public duration;
-
     // Variable for re-entrancty attack check.
     bool locked = false;
 
@@ -42,16 +36,12 @@ contract SecureVault
 
     // Emitted when the contract is initially deployed.
     event CreateVault(address indexed __owner, uint256 indexed __time);
-
     // Emitted when new money is added into the vault.
     event DepositToVault(address indexed __owner, uint256 indexed __amount);
-
     // Emitted when the owner prolongs the vault duration by some seconds.
     event ProlongVault(address indexed __owner, uint256 indexed __time);
-
     // Emitted when the owner withdraws money from the vault.
     event WithdrawFromVault(address indexed __owner, uint256 indexed __amount);
-
     // Emitted when the vault is destroyed.
     event DestroyVault(address indexed _owner, uint256 indexed __time);
 
@@ -73,11 +63,9 @@ contract SecureVault
     {
         // Makes sure the locked is false.
         require(!locked, "No ReEntrance");
-
         // Sets the locked to true so that a new entrance breaks on line 70.
         locked = true;
         _;
-
         // Change locked back to false after the function is through with execution.
         locked = false;
     }
@@ -104,7 +92,6 @@ contract SecureVault
     {
         // Assign the owner to whoever deploys the contract.
         owner = payable(msg.sender);
-
         // Emit the CreateVault event.
         emit CreateVault(msg.sender, block.timestamp);
     }
@@ -120,13 +107,10 @@ contract SecureVault
     {
         // Use SafeMath to add the msg.value to the total_funds.
         total_funds = total_funds.add(msg.value);
-
         // Pushes the duration by the default duration.
         duration = block.timestamp.add(DEFAULT_DURATION);
-
         // Emit the DepositToVault event.
         emit DepositToVault(owner, msg.value);
-
         // Emit the ProlongVault event.
         emit ProlongVault(owner, DEFAULT_DURATION);
     }
@@ -145,27 +129,20 @@ contract SecureVault
     {
         // Check the time and make sure that the current time has passed the time.
         require(block.timestamp >= duration, "Time is not yet up");
-
         // Make sure that the amount is greater than 0.
         require(__withdraw_amount > 0, "Amount <= 0");
-
         // Makes sure that the amount to withdraw is less than the total funds in the contract.
         require(__withdraw_amount <= total_funds, "Amount > Total funds");
-
         // Hold the total funds in a memory storage for validation.
         uint256 total_funds_validator = total_funds;
-
         // Remove the value from the total funds in the contract.
         total_funds = total_funds.sub(__withdraw_amount);
-
         // Send the value to the  owner of the contract.
         // Since msg.sender == owner and is payable.
         owner.transfer(__withdraw_amount);
-
         // Assert that the total_funds have changed.
         // After every transfer, the total funds will be lower than the initial storage of the total funds validator.
         assert(total_funds < total_funds_validator);
-
         // Emit the WithdrawFromVault event.
         emit WithdrawFromVault(owner, __withdraw_amount);
     }
@@ -181,23 +158,17 @@ contract SecureVault
     {
         // Check the time and make sure that the current time has passed the time.
         require(block.timestamp >= duration, "Time is not yet up");
-
         // Makes sure that the account is not empty.
         require(total_funds > 0, "Total funds == 0");
-
         // Hold the total funds in a memory storage for validation.
         uint256 stored_total_funds = total_funds;
-
         // Remove the value from the total funds in the contract.
         total_funds = total_funds.sub(stored_total_funds);
-
         // Send the value to the  owner of the contract.
         // Since msg.sender == owner and is payable.
         owner.transfer(stored_total_funds);
-
         // Assert that the total_funds have changed and is now == 0.
         assert(total_funds == 0);
-
         // Emit the WithdrawFromVault event.
         emit WithdrawFromVault(owner, stored_total_funds);
     }
